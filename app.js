0222ed8b39ea9b528818b55894938af45098c73e
PhilipeF <philipesferreiraa@gmail.com>
2024-01-30 20:59:28 -0300

    Feat: Projeto ok

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
 const form = document.querySelector('.form-cadastrar');
+const nomeContato = document.querySelector('.nome-contato');
+const numeroContato = document.querySelector('.numero-contato');
+const tbody = document.querySelector('tbody');
+
+function cadatrarContato() {
+    if (nomeContato.value.trim() === '' || numeroContato.value.trim() === '') {
+        alert('Por favor, preencha todos os campos');
+        return;
+    }
+    tbody.innerHTML += `
+        <tr>
+            <td>${nomeContato.value}</td>
+            <td>${numeroContato.value} </td>
+        </tr>
+
+    `
+}
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    console.log('Passei aqui')
+    cadatrarContato();
 
+    nomeContato.value = '';
+    numeroContato.value = '';
 })
+
+
